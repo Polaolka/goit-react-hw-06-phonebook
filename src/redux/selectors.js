e75d25523a4a0c37299f24bde99c60434f2259a3
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-04 15:16:18 +0300

    optimise selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,10 +1,11 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.contacts;
 export const selectContactFilter = state => state.filter;
 
-export const selecVisibleContacts = state => {
-    const contacts = selectContacts(state);
-    const filter = selectContactFilter(state);
-    return contacts.filter(contact =>
-        contact.name.toLowerCase().includes(filter)
-      );
-}
+export const selecVisibleContacts = createSelector(
+  [selectContacts, selectContactFilter],
+  (tasks, filter) => {
+    return tasks.filter(contact => contact.name.toLowerCase().includes(filter));
+  }
+);
